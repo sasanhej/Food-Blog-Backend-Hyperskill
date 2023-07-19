cfd18f5f902fbe5c128aa4b1f59201eb1e135d8b
Sasan Hejrani <s.hejrani@gmail.com>
2023-07-19 23:48:54 +0200

    Stage 2 Commit

--- Food Blog Backend/task/blog.py
+++ Food Blog Backend/task/blog.py
@@ -1,9 +1,11 @@
 import sqlite3
 import sys
 
+
 args = sys.argv
 #dbname = args[1]
 dbname = 'food_blog.db'
+
 
 conn = sqlite3.connect(dbname)
 cur = conn.cursor()
@@ -28,8 +30,16 @@ measure_name TEXT UNIQUE
 );
 ''')
 conn.commit()
+cur.execute('''
+CREATE TABLE IF NOT EXISTS recipes(
+recipe_id INTEGER PRIMARY KEY,
+recipe_name TEXT NOT NULL,
+recipe_description TEXT
+);
+''')
+conn.commit()
 
-'''
+
 # Stage 1/5
 data = {"meals": ("breakfast", "brunch", "lunch", "supper"),
         "ingredients": ("milk", "cacao", "strawberry", "blueberry", "blackberry", "sugar"),
@@ -39,5 +49,15 @@ for j in data:
     for i in data[j]:
         cur.execute(f'INSERT INTO {j} ({j[:-1]}_name) VALUES ("{i}");')
         conn.commit()
+
+# Stage 2/5
+print('Pass the empty recipe name to exit.')
+while True:
+    recipe_name = input('Recipe name:')
+    if recipe_name == "":
+        break
+    recipe_description = input('Recipe description:')
+    cur.execute(f'INSERT INTO recipes(recipe_name, recipe_description) VALUES (?,?);',
+                (recipe_name, recipe_description))
+    conn.commit()
 conn.close()
-'''
